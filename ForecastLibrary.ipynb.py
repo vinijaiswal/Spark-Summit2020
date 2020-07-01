14dafcc05adbd3b8a7334f070ce077b5918509f7
vinijaiswal <>
2020-07-01 19:06:38 +0000

    Test demo for wildlife

--- ForecastLibrary.ipynb.py
+++ ForecastLibrary.ipynb.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 from fbprophet import Prophet
+import seaborn
 
 
 # COMMAND ----------
---
+++ test-add-integration.py
@@ -0,0 +1,2 @@
+# Databricks notebook source
+# MAGIC %md This is a test
